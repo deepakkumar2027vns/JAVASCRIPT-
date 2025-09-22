0f3e2dde80cdb18adfc2b15330259750701e92f5
Deepak Kumar <deepakvns2027@gmail.com>
2025-09-22 06:30:50 +0000

    the

---
+++ CLASS02/01string.js
@@ -0,0 +1,13 @@
+function getlengthstring(str){
+    console.log("Sring:",str)
+    console.log("string length:",str.length)
+    
+}
+getlengthstring("hello deepak")
+
+function findindexof(str,target){
+    console.log("original str:",str)
+    console.log("index of :",str.indexOf(target))
+}
+
+findindexof("hellow i am deepak","deepak")
